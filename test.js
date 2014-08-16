6eee7c15e5ce1c35673a7cea8a14816110559abc
Matt Brennan <mattyb1000@gmail.com>
2014-08-16 08:13:03 +0100

    mo tests

--- test.js
+++ test.js
@@ -2,19 +2,25 @@ var cajole = require('./index.js');
 var expect = require('expect.js');
 
 exports.Cajole = {
-	'should convert strings': function() {
-		var convert = cajole(String);
-		expect(convert({toString: function() {return 'hello'}})).to.be('hello');
+	'should convert strings': {
+		'from toString objects': function() {
+			var convert = cajole(String);
+			expect(convert({toString: function() {return 'hello'}})).to.be('hello');
+		},
 	},
 	'should convert numbers': function() {
 		var convert = cajole(Number);
 		expect(convert('5')).to.be(5);
 		expect(convert('5.5')).to.be(5.5);
+		expect(convert(5)).to.be(5);
+		expect(convert(5.5)).to.be(5.5);
 	},
 	'should convert integers': function() {
 		var convert = cajole('integer');
 		expect(convert('5')).to.be(5);
 		expect(convert('5.5')).to.be(5);
+		expect(convert(5)).to.be(5);
+		expect(convert(5.5)).to.be(5);
 	},
 	'should traverse objects': {
 		'single level': function() {
