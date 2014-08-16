591c14d8899189b3cd61da01aa9d3ff51f2a706b
Matt Brennan <mattyb1000@gmail.com>
2014-08-16 08:07:31 +0100

    test deep traversal

--- test.js
+++ test.js
@@ -11,18 +11,43 @@ exports.Cajole = {
 		expect(convert('5')).to.be(5);
 		expect(convert('5.5')).to.be(5.5);
 	},
-	'should traverse objects': function() {
-		var convert = cajole({
-			foo: String,
-			bar: Number
-		});
+	'should traverse objects': {
+		'single level': function() {
+			var convert = cajole({
+				foo: String,
+				bar: Number
+			});
 
-		expect(convert({
-			foo: {toString: function() {return 'hello'}},
-			bar: '5'
-		})).to.eql({
-			foo:'hello',
-			bar:5
-		});
+			expect(convert({
+				foo: {toString: function() {return 'hello'}},
+				bar: '5'
+			})).to.eql({
+				foo:'hello',
+				bar:5
+			});
+		},
+		'deeper': function() {
+			var convert = cajole({
+				foo: {
+					bar: String,
+					baz: Number
+				},
+				quux: Number
+			});
+
+			expect(convert({
+				foo: {
+					bar: 'hello',
+					baz: '5.5'
+				},
+				quux: '5'
+			})).to.eql({
+				foo: {
+					bar: 'hello',
+					baz: 5.5
+				},
+				quux: 5
+			});
+		}
 	}
 };
